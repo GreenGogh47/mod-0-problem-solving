f9b86aa8cfc14613ccaacfe48f900b8c4c6a7e2a
Chris Crane <chrisjcrane@gmail.com>
2022-11-04 13:15:25 -0600

    Add challenge 6 work and notes.

--- challenge_6.rb
+++ challenge_6.rb
@@ -1,19 +1,27 @@
+# Given a sentence with only lowercase letters,
+# print the same sentence with the first letter
+# of every word capitalized.
+# For example, if you were given "Turing is the best",
+# return "Turing Is The Best" instead!
+
+
+# # Overall goal
+# Make it so any sentence given
+# will return with the first letter
+# of every word capitalized
+
+
+# # Pseudocode
+# 1. Define a string with a variable
+# 2. capitalize every words first letter with the
+# capitalize method
+# 3. print the result
 
 
 
+# Final Solution (in progress)
+# If I were on rails I could just use .titleize lol
 
+string = "You are my sunshine, my only sunshine. You make me happy when skies are grey."
+p string.uppercase.first
 
-
-
-
-# Overall goal
-
-
-
-
-# Pseudocode
-
-
-
-
-# Final Solution
